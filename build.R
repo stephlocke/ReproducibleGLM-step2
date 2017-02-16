368bb26e9a9771645d0ce517a09bc58dc310f6fb
Steph Locke <steph@itsalocke.com>
2017-02-16 19:53:20 +0000

    Alt loc

--- build.R
+++ build.R
@@ -1,6 +1,6 @@
+sessionInfo()
 packrat::on()
 packrat::restore()
-sessionInfo()
 installed.packages()
 rmarkdown::render("README.Rmd", output_format = rmarkdown::github_document(), output_dir="docs")
 rmarkdown::render("outputs/FullModel.Rmd", output_dir="docs")
